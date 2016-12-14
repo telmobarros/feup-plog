9d54ba5b18a025e9730654f229ac41ac8311261b
MartaTorgal <up201407727@fe.up.pt>
2016-12-14 11:37:39 +0000

    Add files via upload

--- TP2/main.pl
+++ TP2/main.pl
@@ -10,9 +10,15 @@ horario(3, [[1,3,4,2],[2,4,8,9,1],[1,2,8],[8,4,5],[2,5,7,6,9]]).
 horario(4, [[2,4,8,9,1],[1,3,4,2],[8,4,5],[2,5,7,6,9],[1,2,8]]).
 
 
-ano_escolar(NDisciplinas, NTurmas, NSemanas,ListaTestes1, ListaTestes2):-
+ano_escolar(NDisciplinas, NTurmas, NSemanas, NTPCDia, NTPCDisc, ListaTestes1, ListaTestes2):-
 	criar_lista_testes(NDisciplinas, NTurmas, ListaTestes1),
 	criar_lista_testes(NDisciplinas, NTurmas, ListaTestes2),
+	criar_lista_tpcs(NDisciplinas,Lista),
+	getTPCs(Lista,ListaTpcs),
+	domain(ListaTpcs,0,1),
+	horario(1,Horario1),
+	tpcEmDiaComDisciplina(Horario1,Lista),
+	setSomaTpcDia(NTPCDia,Lista),
 
 	%calcula semanas em que pode haver teste e define os dominios das duas fases de testes
 	NSemanas >= NDisciplinas,
@@ -33,11 +39,13 @@ ano_escolar(NDisciplinas, NTurmas, NSemanas,ListaTestes1, ListaTestes2):-
 	%impoe testes na mesma semana com distancia de 2 dias
 	testesNaoConsecutivos(ListaTestes1, ListaTestes1),
 	testesNaoConsecutivos(ListaTestes2, ListaTestes2),
-
+	
 	labeling([],ListaTestes1),
 	labeling([],ListaTestes2),
+	labeling([max],ListaTpcs),
         writeHorarios(1, NTurmas),
-        writeTestes(ListaTestes1, ListaTestes2).
+        writeTestes(ListaTestes1, ListaTestes2),
+		write(Lista).
 
 
 %%%criador da lista inicial de testes para cada fase de testes%%%%%%%%%%%%%%%%%%%%%%
@@ -61,7 +69,25 @@ criar_lista_testes_aux(DisciplinaCounter, TurmaCounter, NDisciplinas, NTurmas, [
 	NextDisciplinaCounter is DisciplinaCounter + 1,
 	criar_lista_testes_aux(NextDisciplinaCounter , TurmaCounter, NDisciplinas, NTurmas, Ls).
 
+criar_lista_tpcs(NDisciplinas,Lista):-
+	once(criar_lista_tpcs_aux(1, 1, NDisciplinas, Lista)).
 
+criar_lista_tpcs_aux(6,1,_,[]).
+
+criar_lista_tpcs_aux(Ndia,NDisciplinas,NDisciplinas,[Ndia,NDisciplinas,_|Ls]):-
+    Ndia =< 5,
+	NdiaVar is Ndia +1,
+	criar_lista_tpcs_aux(NdiaVar,1,NDisciplinas,Ls).
+	
+criar_lista_tpcs_aux(Ndia,Ndisc,NDisciplinas,[Ndia,Ndisc,_|Ls]):-
+Ndisc < NDisciplinas,
+Ndia =< 5,
+NdiscVar is Ndisc +1,
+criar_lista_tpcs_aux(Ndia,NdiscVar,NDisciplinas,Ls).
+
+
+
+	
 %%Dominios de semanas(meio e final do periodo) e dias da semana
 
 impoeDominiosTeste(Lista, MinSemana, MaxSemana):-
@@ -73,6 +99,11 @@ impoeDominiosTeste(Lista, MinSemana, MaxSemana):-
 getSemanas_DiasSemana([], [], []).
 getSemanas_DiasSemana([_, _, Semana , DiaSemana | Ls], [Semana | Ss], [DiaSemana | Ds]):-
 	getSemanas_DiasSemana(Ls, Ss, Ds).
+
+getTPCs([],[]).
+getTPCs([_,_,L1|Ls],[L1|Ts]):-
+getTPCs(Ls,Ts).
+
 
 %%Dias de testes apenas em dias em que a disciplina esta no horario
 
@@ -95,8 +126,24 @@ testesEmDiasComDisciplinaAux([H1|Hs], Disciplina, DiaCounter, DiaSemana):-
         NextDiaCounter is DiaCounter + 1,
         testesEmDiasComDisciplinaAux(Hs, Disciplina, NextDiaCounter, DiaSemana).
 
-                       
-        
+
+tpcEmDiaComDisciplina(_,[]).		
+ tpcEmDiaComDisciplina(Horario,[Ndia,Ndisc,Var|Ls]):-       
+	once(tpcEmDiaComDisciplinaAux(Horario,Ndia,Ndisc, Var,1)),
+		tpcEmDiaComDisciplina(Horario,Ls).
+		
+	
+tpcEmDiaComDisciplinaAux([L1|Ls],Ndia,Ndisc,Var,NdiaCounter):-
+NdiaCounter < Ndia,
+NdiaVar is NdiaCounter +1,
+tpcEmDiaComDisciplinaAux(Ls,Ndia,Ndisc,Var,NdiaVar).
+
+tpcEmDiaComDisciplinaAux([L1|Ls],Ndia,Ndisc,Var,Ndia):-
+\+member(Ndisc,L1),
+Var #= 0.
+
+tpcEmDiaComDisciplinaAux([L1|Ls],Ndia,Ndisc,Var,Ndia):-
+member(Ndisc,L1).
 
 
 %%%Restricao testes nao consecutivos%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -148,3 +195,25 @@ impoeDoisTestesCadaSemana(Semanas, MinSemana, MaxSemana):-
 	count(MinSemana,Semanas,#=<,2),
 	NextMinSemana is MinSemana + 1,
 	impoeDoisTestesCadaSemana(Semanas, NextMinSemana, MaxSemana).
+	
+	
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Soma do numero de tpcs por dia %%%%%%%%%%%%%%%%%%%%%%%%
+
+
+setSomaTpcDia(NTPCDia,Lista):-
+setSomaTpcDiaAux(Lista,L1,L2,L3,L4,L5),
+sum(L1,#=<,NTPCDia),
+sum(L2,#=<,NTPCDia),
+sum(L3,#=<,NTPCDia),
+sum(L4,#=<,NTPCDia),
+sum(L5,#=<,NTPCDia).
+%sum(L1,#=,0).% #\/ sum(L2,#=,0).% #\/ sum(L3,#=,0) #\/ sum(L4,#=,0) #\/ sum(L5,#=,0).
+
+setSomaTpcDiaAux([1,_,Var|Ls],[Var|Ts],L2,L3,L4,L5):-setSomaTpcDiaAux(Ls,Ts,L2,L3,L4,L5).
+setSomaTpcDiaAux([2,_,Var|Ls],[],[Var|Ts],L3,L4,L5):-setSomaTpcDiaAux(Ls,[],Ts,L3,L4,L5).
+setSomaTpcDiaAux([3,_,Var|Ls],[],[],[Var|Ts],L4,L5):-setSomaTpcDiaAux(Ls,[],[],Ts,L4,L5).
+setSomaTpcDiaAux([4,_,Var|Ls],[],[],[],[Var|Ts],L5):-setSomaTpcDiaAux(Ls,[],[],[],Ts,L5).
+setSomaTpcDiaAux([5,_,Var|Ls],[],[],[],[],[Var|Ts]):-setSomaTpcDiaAux(Ls,[],[],[],[],Ts).
+setSomaTpcDiaAux([],[],[],[],[],[]).
+
+
